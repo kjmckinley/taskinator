921860558e489bf921477f9b3d594567f4eb6219
Kyle <kj.mckinley@gmail.com>
2021-03-19 01:48:48 -0500

    added ability to add a task

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,6 +1,17 @@
 var buttonEl = document.querySelector("#save-task");
 console.log(buttonEl);
 
-buttonEl.addEventListener("click", function() {
-    alert("button clicked");
-  });
+var listTaskEl = document.querySelector('#tasks-to-do');
+
+var createTaskHandler = function(){
+  var newTaskEl = document.createElement('li');
+  newTaskEl.className = "task-item";
+  newTaskEl.textContent = "New Task";
+  listTaskEl.appendChild(newTaskEl);
+};
+
+buttonEl.addEventListener("click", createTaskHandler);
+
+
+
+  
